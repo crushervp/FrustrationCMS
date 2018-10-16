8cf652e92b660ef04c05df3d497663938ba9df76
chrislynch42 <chrislynch42@yahoo.com>
2018-10-16 13:04:30 -0400

    Reset working directory after unit test

--- maven/src/test/java/com/codeexcursion/cms/service/GetConfigurationTest.java
+++ maven/src/test/java/com/codeexcursion/cms/service/GetConfigurationTest.java
@@ -2,6 +2,7 @@ package com.codeexcursion.cms.service;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 
 import java.io.File;
@@ -10,12 +11,14 @@ import java.nio.file.Paths;
 import org.apache.commons.configuration2.Configuration;
 
 public class GetConfigurationTest {
-
+  private String origWd;
+  private static final String USER_DIR = "user.dir";
 @Before
-public void setPwd() {
+public void setCwd() {
+  origWd = System.getProperty(USER_DIR);  
   File baseDir = Paths.get("src/test/artifacts").toFile();
   String path = baseDir.getAbsolutePath();
-  System.setProperty("user.dir", baseDir.getAbsolutePath());
+  System.setProperty(USER_DIR, baseDir.getAbsolutePath());
 }
 
 @Test
@@ -24,5 +27,9 @@ public void getTest() {
   Assert.assertEquals("Title should be \"Test Configuration\"", "Test Configuration", configuration.getString("title"));
 }
 
+@After
+public void resetCwd() {  
+  System.setProperty(USER_DIR, origWd);
+}
 
 }
