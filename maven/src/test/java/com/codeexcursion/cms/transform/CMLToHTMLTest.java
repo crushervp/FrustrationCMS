03edb2cce6b0930373593ecdfee9d94f8b54ea67
chrislynch42 <chrislynch42@yahoo.com>
2018-10-17 11:36:25 -0400

    Testing modifying html using Jsoup

--- maven/src/test/java/com/codeexcursion/cms/transform/CMLToHTMLTest.java
+++ maven/src/test/java/com/codeexcursion/cms/transform/CMLToHTMLTest.java
@@ -11,11 +11,13 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.jsoup.nodes.Element;
 import org.jsoup.Jsoup;;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.nodes.Node;
 
 public class CMLToHTMLTest {
 
     @Test
-    public void jsoupTest() {
+    public void jsoupSelectTest() {
         File htmlFile = Paths.get("src/test/artifacts/content/post/2018/getting-tiger-vnc-to-work-on-fedora-28.html")
                 .toFile();
 
@@ -23,7 +25,7 @@ public class CMLToHTMLTest {
             Document html = Jsoup.parse(htmlFile, "UTF-8");
 
             Elements metaTags = html.select("meta");
-            Assert.assertTrue("Did not find multiple meta tags.", metaTags.size() == 6);
+            Assert.assertTrue("Did not find multiple meta tags.", metaTags.size() > 5);
             
             Elements terminals = html.select("terminal");
             Assert.assertTrue("Did not find multiple terminal tags.", terminals.size() > 6);
@@ -31,6 +33,49 @@ public class CMLToHTMLTest {
             Elements metaType = html.select("meta[name=type]");
             Assert.assertTrue("Did not find meta tag of type.", metaType.size() == 1);
             Assert.assertEquals("Did not find meta tag of type.", "post", metaType.get(0).attr("content"));
+
+
+        } catch (IOException exception) {
+            Assert.fail("Unable to find file " + htmlFile.getAbsolutePath());
+        }
+    }
+
+    @Test
+    public void jsoupChangeTest() {
+        File htmlFile = Paths.get("src/test/artifacts/content/post/2018/getting-tiger-vnc-to-work-on-fedora-28.html")
+                .toFile();
+
+        try {
+            Document html = Jsoup.parse(htmlFile, "UTF-8");
+
+            Elements terminals = html.select("terminal");
+            Assert.assertTrue("Did not find multiple terminal tags.", terminals.size() > 6);
+
+            Elements captions = html.select("span[class=codeCaption]");
+            Assert.assertTrue("Should not find any code captions.", captions.size() < 1);
+
+            Elements preTerminals = html.select("pre[class=terminal]");
+            Assert.assertTrue("Should not find any code captions.", preTerminals.size() < 1);
+
+            Element terminal = terminals.get(0);
+            String caption = terminal.attr("caption");
+            Assert.assertEquals("Did not have correct caption.", "Stop Tiger VNC", caption );
+
+            Element captionNode = new Element("span");
+            captionNode.attr("class", "codeCaption");
+            captionNode.appendChild(new TextNode(caption));
+            terminal.before(captionNode);
+
+            Element terminalNode = new Element("pre");
+            terminalNode.attr("class", "terminal");
+            terminalNode.appendChild(new TextNode(terminal.html()));
+            terminal.replaceWith(terminalNode);
+
+            captions = html.select("span[class=codeCaption]");
+            Assert.assertTrue("Should not find any code captions.", captions.size() > 0);
+
+            preTerminals = html.select("pre[class=terminal]");
+            Assert.assertTrue("Should not find any code captions.", preTerminals.size() > 0);
 
 
         } catch (IOException exception) {
