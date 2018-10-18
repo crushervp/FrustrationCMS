1ae8ff81018b1dfaf6d41ea872e04144de9a7295
chris <chris@yahoo.com>
2018-10-18 10:04:10 -0400

    Flexmark test working with YAML front matter

--- maven/src/test/java/com/codeexcursion/cms/transform/CMLToHTMLTest.java
+++ maven/src/test/java/com/codeexcursion/cms/transform/CMLToHTMLTest.java
@@ -14,13 +14,15 @@ import org.jsoup.select.Elements;
 import org.jsoup.nodes.Element;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.TextNode;
-import org.jsoup.nodes.Node;
 
-import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
-import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.ext.yaml.front.matter.AbstractYamlFrontMatterVisitor;
+import com.vladsch.flexmark.ext.yaml.front.matter.YamlFrontMatterExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.options.MutableDataSet;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class CMLToHTMLTest {
 
@@ -96,7 +98,7 @@ public class CMLToHTMLTest {
         Path markdownFile = Paths.get("src/test/artifacts/content/post/2018/liferay-sybase-to-oracle-data-migration.md");
 
         MutableDataSet options = new MutableDataSet();
-        
+        options.set(Parser.EXTENSIONS, Arrays.asList(YamlFrontMatterExtension.create()));
         // uncomment to set optional extensions
         //options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(), StrikethroughExtension.create()));
 
@@ -109,21 +111,28 @@ public class CMLToHTMLTest {
             String markdown = new String(Files.readAllBytes(markdownFile));
             // You can re-use parser and renderer instances
             com.vladsch.flexmark.ast.Document markdownDocument = parser.parse(markdown);
+            AbstractYamlFrontMatterVisitor yamlVisitor = new AbstractYamlFrontMatterVisitor();
+            yamlVisitor.visit(markdownDocument);
+            Map<String, List<String>> metaData = yamlVisitor.getData();            
+            Assert.assertTrue("Did not find meta data.", metaData.size() > 5);
+            Assert.assertNotNull("Did not find type meta data.", metaData.get("type"));
+            System.out.println(metaData.get("type"));
+            System.out.println(metaData.get("type").get(0));
+            System.out.println(metaData.get("type").get(0));
+            System.out.println(metaData.get("type").get(0));
+            System.out.println(metaData.get("type").get(0));
+            System.out.println(metaData.get("type").get(0));
+            Assert.assertEquals("Did not find type meta data.", "'post'", metaData.get("type").get(0));
+            
+            
+            
             String markdownHtml = renderer.render(markdownDocument);  
             System.out.println(markdownHtml);
 
             Document html = Jsoup.parse(markdownHtml, "UTF-8");
 
-            Elements metaTags = html.select("meta");
-            Assert.assertTrue("Did not find multiple meta tags.", metaTags.size() > 5);
-            
-            Elements terminals = html.select("terminal");
-            Assert.assertTrue("Did not find multiple terminal tags.", terminals.size() > 6);
-
-            Elements metaType = html.select("meta[name=type]");
-            Assert.assertTrue("Did not find meta tag of type.", metaType.size() == 1);
-            Assert.assertEquals("Did not find meta tag of type.", "post", metaType.get(0).attr("content"));
-
+            Elements codes = html.select("pre[class=prettifyprint]");
+            Assert.assertTrue("Did not find multiple pre tags containing code.", codes.size() > 3);
 
         } catch (IOException exception) {
             Assert.fail("Unable to find file " + markdownFile.toAbsolutePath());
