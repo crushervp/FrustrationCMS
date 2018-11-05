ebab532326157f541ad67eec05341123cdfb33f2
chrislynch42 <chrislynch42@yahoo.com>
2018-11-05 14:46:43 -0500

    Adding code to transform terminal and code tags to html.  Production emergency resulted in not fixing unit tests

--- maven/src/main/java/com/codeexcursion/cms/transform/HTML.java
+++ maven/src/main/java/com/codeexcursion/cms/transform/HTML.java
@@ -5,12 +5,59 @@
  */
 package com.codeexcursion.cms.transform;
 
+import java.util.Optional;
+import java.util.Objects;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+import org.jsoup.Jsoup;
+
 /**
  *
- * @author chris
- * Purpose of this class is to manipulate HTML documents to produce
- * Frustration HTML Documents.
+ * @author chris Purpose of this class is to manipulate HTML documents to
+ *         produce Frustration HTML Documents.
  */
 public final class HTML {
-    
+
+    public static final Optional<String> transform(String html) {
+        String returnValue = null;
+        if(html != null) {
+            Document htmlDocument = Jsoup.parse(html);
+            Elements terminals = htmlDocument.select("terminal");
+            Optional.ofNullable(terminals).ifPresent(HTML::handleTerminals);
+
+            Elements codeBlocks = htmlDocument.select("code");
+            Optional.ofNullable(codeBlocks).ifPresent(HTML::handleCodeBlocks);
+
+        }
+        return Optional.ofNullable(returnValue);
+    }
+
+
+    private final static void handleTerminals(Elements terminals) {
+      terminals.stream().filter(Objects::nonNull).forEach(HTML::replaceTerminal);
+    }
+  
+
+    private final static void handleCodeBlocks(Elements codeBlocks) {
+        codeBlocks.stream().filter(Objects::nonNull).forEach(HTML::replaceCodeBlocks);
+    }
+
+    private final static void replaceTerminal(Element currentElement) {
+        replace(currentElement, "terminal");
+    }
+
+    private final static void replaceCodeBlocks(Element currentElement) {
+        replace(currentElement, "prettifyprint");
+    }
+
+    private final static void replace(Element currentElement, String cssClass) {
+        Tags.getCaption(currentElement).ifPresent(currentElement::before);
+        replace(currentElement, Tags.getContent(currentElement, cssClass));
+    }    
+
+    private final static void replace(Element currentElement, Element newElement) {
+      Optional.of(newElement).ifPresent(currentElement::replaceWith);
+    }
+
 }
--- maven/src/main/java/com/codeexcursion/cms/transform/Tags.java
+++ maven/src/main/java/com/codeexcursion/cms/transform/Tags.java
@@ -8,27 +8,24 @@ import org.jsoup.nodes.TextNode;
 public final class Tags {
 
     public final static Optional<Element> getCaption(Element terminal) {
+        Optional.ofNullable(terminal).orElseThrow(IllegalArgumentException::new);
         Element captionNode = null;
-        if (terminal != null) {
-            String caption = terminal.attr("caption");
-            if (caption != null && !caption.isEmpty()) {
-                captionNode = new Element("span");
-                captionNode.attr("class", "codeCaption");
-                captionNode.appendChild(new TextNode(caption));
-            }
+        String caption = terminal.attr("caption");
+        if (caption != null && !caption.isEmpty()) {
+            captionNode = new Element("span");
+            captionNode.attr("class", "codeCaption");
+            captionNode.appendChild(new TextNode(caption));
         }
         return Optional.ofNullable(captionNode);
     }
 
-    public final static Optional<Element> getContent(Element node, String cssClass) {
-        Element returnNode = null;
+    public final static Element getContent(Element node, String cssClass) {
+        Optional.ofNullable(node).orElseThrow(IllegalArgumentException::new);
         cssClass = Optional.ofNullable(cssClass).orElseGet(() -> "");
-        if (node != null) {
-            returnNode = new Element("pre");
-            returnNode.attr("class", cssClass);
-            returnNode.appendChild(new TextNode(node.html()));
-        }
-        return Optional.ofNullable(returnNode);
+        Element returnNode = new Element("pre");
+        returnNode.attr("class", cssClass);
+        returnNode.appendChild(new TextNode(node.html()));
+        return returnNode;
     }
     
 
